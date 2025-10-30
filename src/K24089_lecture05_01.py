b8eb032cc206a8c2eeec1fc79b837d8492a62a07
raimu7 <k24089kk@aitech.ac.jp>
2025-10-30 15:32:45 +0900

    update

--- src/K24089_lecture05_01.py
+++ src/K24089_lecture05_01.py
@@ -10,7 +10,7 @@ def K24089_lecture05_01():
 
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
+    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') 
     # capture_img : cv2.Mat = "implement me"
 
     g_hight, g_width, g_channel = google_img.shape
