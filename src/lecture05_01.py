e12681679ebf0b537d6d297dca3188c8d0c2fb52
kosuke361 <mars.1.881@icloud.com>
2025-10-30 15:30:29 +0900

    グループワーク

--- src/lecture05_01.py
+++ src/lecture05_01.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from my_module.K21999.lecture05_camera_image_capture import MyVideoCapture
+from my_module.K24100.lecture05_camera_image_capture import MyVideoCapture
 
 def lecture05_01():
 
@@ -10,8 +10,7 @@ def lecture05_01():
 
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
-    # capture_img : cv2.Mat = "implement me"
+    capture_img : cv2.Mat = app.run()
 
     g_hight, g_width, g_channel = google_img.shape
     c_hight, c_width, c_channel = capture_img.shape
@@ -24,8 +23,8 @@ def lecture05_01():
             # もし白色(255,255,255)だったら置き換える
             if (b, g, r) == (255, 255, 255):
                 pass
-                #implement me
+                google_img[y, x] = capture_img[y, x]
 
     # 書き込み処理
-    # implement me
-
+    cv2.imwrite('output_images/lecture05_01_k24100.png')
+    
